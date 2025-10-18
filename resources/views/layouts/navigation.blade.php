5b41f2501ad0aac1b92dfe75365b9172da3cef8a
QuadDarv1ne <maksimqwe42@mail.ru>
2025-10-18 18:16:06 +0300

    modified:   resources/views/dashboard.blade.php 	modified:   resources/views/layouts/navigation.blade.php 	modified:   routes/web.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,11 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <!-- ДОБАВЛЕНО: Ссылка на задачи -->
+                    <x-nav-link :href="route('tasks.index')" :active="request()->routeIs('tasks.*')">
+                        {{ __('Задачи') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -69,6 +74,11 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+
+            <!-- ДОБАВЛЕНО: Мобильная ссылка на задачи -->
+            <x-responsive-nav-link :href="route('tasks.index')" :active="request()->routeIs('tasks.*')">
+                {{ __('Задачи') }}
             </x-responsive-nav-link>
         </div>
 
--- routes/web.php
+++ routes/web.php
@@ -2,28 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
 
-// Перенаправление с главной страницы на задачи (для авторизованных)
+// Главная → редирект на задачи для авторизованных
 Route::get('/', function () {
     return redirect()->route('tasks.index');
 })->middleware('auth');
 
+// Dashboard (требует подтверждённой почты)
 Route::get('/dashboard', [DashboardController::class, 'index'])
     ->name('dashboard')
     ->middleware(['auth', 'verified']);
-    
-// Группа маршрутов задач
-Route::middleware('auth')->group(function () {
-    Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
-    Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
-    Route::patch('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
-    Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
 
+// Группа защищённых маршрутов
+Route::middleware(['auth'])->group(function () {
+    // Задачи
+    Route::resource('tasks', TaskController::class)->except(['show', 'create', 'edit']);
+
+    // Профиль (Breeze-совместимость)
     Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    // (опционально) Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+// Аутентификация (Breeze)
 require __DIR__.'/auth.php';
