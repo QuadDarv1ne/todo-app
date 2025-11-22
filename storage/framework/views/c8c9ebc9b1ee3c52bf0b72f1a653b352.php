e9c64316363add557f126b7e947235d463f088d4
QuadDarv1ne <maksimqwe42@mail.ru>
2025-11-22 12:14:07 +0300

    deleted:    storage/framework/views/249abb56c659384e7c4dbc983367d23b.php 	deleted:    storage/framework/views/3399c1a2de1fc49ba1fa28136dcbc296.php 	deleted:    storage/framework/views/3573b62a472b16b11fd9f9d0540f1425.php 	deleted:    storage/framework/views/3bfbdbed47e74453072e74cb0ce13be2.php 	deleted:    storage/framework/views/3e9a40a2fcd378939806d6a74218349d.php 	deleted:    storage/framework/views/4642f6d54222ae80c2aebf64e43c7f5e.php 	deleted:    storage/framework/views/482cba7205a0eec70f09697e74fa4ff6.php 	deleted:    storage/framework/views/4bf60e93f4bfccaa2bbe1cc2b4307b1a.php 	deleted:    storage/framework/views/596a5d5810b81350806b5aece96cc6d1.php 	deleted:    storage/framework/views/6809ac5f021b7059ed71e68745882d16.php 	deleted:    storage/framework/views/6eac6d2d1451a06fed87a8866ecf345a.php 	deleted:    storage/framework/views/778996eae68e0f34b84b17721f44d381.php 	deleted:    storage/framework/views/8cc3038eeb620013bc7a95e86444d136.php 	deleted:    storage/framework/views/9252e95f091136c35053bddf23027fde.php 	deleted:    storage/framework/views/95acf4fba3993407768a366ad40ec3c6.php 	deleted:    storage/framework/views/9feba9897d165aa96d715acde8a2af4b.php 	deleted:    storage/framework/views/c068dfb5e1880fbae7fd1f396b150ef9.php 	deleted:    storage/framework/views/c8c9ebc9b1ee3c52bf0b72f1a653b352.php 	deleted:    storage/framework/views/cda2a61cd5ac2934ba8364baacabe861.php 	deleted:    storage/framework/views/d8f6fc63a20b61c7b58441d402c8c6c4.php 	deleted:    storage/framework/views/dedbf5261ac40afb30d7aaa06f558546.php 	deleted:    storage/framework/views/e6ea59ba3fd55f17fa097f313572d68e.php 	deleted:    storage/framework/views/e7080065d802128c1f1ad662677c5d0b.php 	deleted:    storage/framework/views/eb08df32c420eb0fe804789945711284.php 	deleted:    storage/framework/views/eca4fdb299f43599a48c439cd78c7433.php 	deleted:    storage/framework/views/fa34ebc938333c32ab8a79d55b334417.php 	deleted:    storage/framework/views/ffb44c6b5349c75cca4490c24d80d780.php

--- storage/framework/views/c8c9ebc9b1ee3c52bf0b72f1a653b352.php
+++
@@ -1,5 +0,0 @@
-<button <?php echo e($attributes->merge(['type' => 'button', 'class' => 'inline-flex items-center px-4 py-2 bg-white dark:bg-gray-700 border border-gray-300 dark:border-gray-600 rounded-md font-semibold text-xs text-gray-700 dark:text-gray-200 uppercase tracking-widest shadow-sm hover:bg-gray-50 dark:hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 dark:focus:ring-indigo-400 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150'])); ?>>
-    <?php echo e($slot); ?>
-
-</button>
-<?php /**PATH C:\Users\maksi\OneDrive\Documents\GitHub\todo-app\resources\views/components/secondary-button.blade.php ENDPATH**/ ?>
